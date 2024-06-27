a318068984cab4709c112a568835eed38de026dc
Daniel Wolf <danielwolf.dev@gmail.com>
2024-06-27 14:40:01 +0200

    update

--- lib/presentation/notes_list/ui/notes_grid_widget.dart
+++ lib/presentation/notes_list/ui/notes_grid_widget.dart
@@ -12,75 +12,148 @@ class NotesGridWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return StaggeredGrid.count(
-        crossAxisCount: 2,
-        mainAxisSpacing: 8,
-        crossAxisSpacing: 8,
-        children: List.generate(notes.length, (index) {
-          final note = notes[index];
-          return Card(
-            shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(
-                Radius.circular(12),
-              ),
+    return MasonryGridView.count(
+      crossAxisCount: 2,
+      mainAxisSpacing: 4,
+      crossAxisSpacing: 4,
+      itemCount: notes.length,
+      itemBuilder: (context, index) {
+        final note = notes[index];
+        return Card(
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(12),
             ),
-            clipBehavior: Clip.antiAlias,
-            child: InkWell(
-              onTap: () {
-                onNote?.call(note);
-              },
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      children: [
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                note.title,
-                                style: Theme.of(context).textTheme.titleLarge,
-                              ),
-                              Text(
-                                note.category,
-                                style: Theme.of(context).textTheme.titleMedium,
-                              ),
-                            ],
-                          ),
+          ),
+          clipBehavior: Clip.antiAlias,
+          child: InkWell(
+            onTap: () {
+              onNote?.call(note);
+            },
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              note.title,
+                              style: Theme.of(context).textTheme.titleLarge,
+                            ),
+                            Text(
+                              note.category,
+                              style: Theme.of(context).textTheme.titleMedium,
+                            ),
+                          ],
                         ),
-                        IconButton.filledTonal(
-                            onPressed: () {
-                              onDelete?.call(note);
-                            },
-                            icon: const Icon(Icons.delete))
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 8,
-                    ),
-                    const Divider(
-                      indent: 0,
-                      endIndent: 0,
-                    ),
-                    const SizedBox(
-                      height: 8,
-                    ),
-                    Flexible(
-                      child: Text(
-                        note.content,
-                        overflow: TextOverflow.ellipsis,
-                        maxLines: 3,
                       ),
+                      IconButton.filledTonal(
+                          onPressed: () {
+                            onDelete?.call(note);
+                          },
+                          icon: const Icon(Icons.delete))
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  const Divider(
+                    indent: 0,
+                    endIndent: 0,
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  Flexible(
+                    child: Text(
+                      note.content,
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 3,
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
-          );
-        }));
+          ),
+        );
+      },
+    );
+
+    // return StaggeredGrid.count(
+    //     crossAxisCount: 2,
+    //     mainAxisSpacing: 8,
+    //     crossAxisSpacing: 8,
+    //     children: List.generate(notes.length, (index) {
+    //       final note = notes[index];
+    //       return Card(
+    //         shape: const RoundedRectangleBorder(
+    //           borderRadius: BorderRadius.all(
+    //             Radius.circular(12),
+    //           ),
+    //         ),
+    //         clipBehavior: Clip.antiAlias,
+    //         child: InkWell(
+    //           onTap: () {
+    //             onNote?.call(note);
+    //           },
+    //           child: Padding(
+    //             padding: const EdgeInsets.all(16.0),
+    //             child: Column(
+    //               mainAxisSize: MainAxisSize.min,
+    //               crossAxisAlignment: CrossAxisAlignment.start,
+    //               children: [
+    //                 Row(
+    //                   children: [
+    //                     Expanded(
+    //                       child: Column(
+    //                         crossAxisAlignment: CrossAxisAlignment.start,
+    //                         children: [
+    //                           Text(
+    //                             note.title,
+    //                             style: Theme.of(context).textTheme.titleLarge,
+    //                           ),
+    //                           Text(
+    //                             note.category,
+    //                             style: Theme.of(context).textTheme.titleMedium,
+    //                           ),
+    //                         ],
+    //                       ),
+    //                     ),
+    //                     IconButton.filledTonal(
+    //                         onPressed: () {
+    //                           onDelete?.call(note);
+    //                         },
+    //                         icon: const Icon(Icons.delete))
+    //                   ],
+    //                 ),
+    //                 const SizedBox(
+    //                   height: 8,
+    //                 ),
+    //                 const Divider(
+    //                   indent: 0,
+    //                   endIndent: 0,
+    //                 ),
+    //                 const SizedBox(
+    //                   height: 8,
+    //                 ),
+    //                 Flexible(
+    //                   child: Text(
+    //                     note.content,
+    //                     overflow: TextOverflow.ellipsis,
+    //                     maxLines: 3,
+    //                   ),
+    //                 ),
+    //               ],
+    //             ),
+    //           ),
+    //         ),
+    //       );
+    //     }));
   }
 }
