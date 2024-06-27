6186c2c5226ed6445d41be0642a2fe67f613e284
Daniel Wolf <danielwolf.dev@gmail.com>
2024-06-27 13:21:13 +0200

    update

--- lib/data/settings/settings_data_source.dart
+++ lib/data/settings/settings_data_source.dart
@@ -3,12 +3,13 @@ import 'package:rx_shared_preferences/rx_shared_preferences.dart';
 class SettingsDataSource {
   final RxSharedPreferences _rxPrefs;
 
+  static const isListViewKey = 'IS_LIST_VIEW';
+
   SettingsDataSource({required RxSharedPreferences prefs}) : _rxPrefs = prefs;
 
-  Stream<bool> isListViewStream() => _rxPrefs.getBoolStream('IS_LIST_VIEW').map(
-        (event) => event ?? true,
-      );
+  Stream<bool> isListViewStream() =>
+      _rxPrefs.getBoolStream(isListViewKey).map((event) => event ?? true);
 
   Future<void> setIsListView(bool value) =>
-      _rxPrefs.setBool('IS_LIST_VIEW', value);
+      _rxPrefs.setBool(isListViewKey, value);
 }
